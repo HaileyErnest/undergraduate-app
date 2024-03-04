5e92c1c155f845d2f67621be84f54405ce4e88b5
HaileyErnest <hernest@calpoly.edu>
2024-03-03 16:50:37 -0800

    shiny link

--- pages/4_Project_3.py
+++ pages/4_Project_3.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
 st.title("Project - Creat Shiny App")
-st.write("check out this [link](https://haileyerneststatistician.shinyapps.io/obesity/)")
+link= "Click on this link to view [link](https://haileyerneststatistician.shinyapps.io/obesity/)"
+st.markdown(link,unsafe_allow_html=True)
+# st.write("check out this [link](https://haileyerneststatistician.shinyapps.io/obesity/)")
