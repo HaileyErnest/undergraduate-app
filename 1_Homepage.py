ff4a9b1a41f7dc84c9e56504acf4ff07d96bd851
HaileyErnest <145716412+HaileyErnest@users.noreply.github.com>
2026-05-26 02:29:52 -0700

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -2,12 +2,11 @@
 from pathlib import Path
 import streamlit as st
 from PIL import Image
-import numpy as np
 
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "HaileyErnest_Resume2026.pdf"
-profile_pic = current_dir / "assets" / "UpdatedpicHailey.PNG"
+profile_pic = current_dir / "assets" / "UpdatedpicHailey.png"
 
 PAGE_TITLE = "Hailey Ernest's Data Portfolio"
 PAGE_ICON = ":wave:"
@@ -35,7 +34,7 @@ with open(resume_file, "rb") as pdf_file:
 # --- HERO SECTION ---
 col1, col2 = st.columns([1, 1.5], gap="small")
 with col1:
-    st.image(str(profile_pic), width=200)
+    st.image(profile_pic, width=200)
 
 with col2:
     st.title(NAME)
