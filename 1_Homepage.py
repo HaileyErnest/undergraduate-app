d921440a442aeb534f5e50bffb07728fb0174b1b
HaileyErnest <145716412+HaileyErnest@users.noreply.github.com>
2026-05-26 02:32:01 -0700

    closer image

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -32,7 +32,7 @@ with open(resume_file, "rb") as pdf_file:
 
 
 # --- HERO SECTION ---
-col1, col2 = st.columns([1, 1.5], gap="small")
+col1, col2 = st.columns([1, 1.2], gap="small")
 with col1:
     st.image(profile_pic, width=200)
 
